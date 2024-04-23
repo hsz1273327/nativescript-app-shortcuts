a910263bcd19116038b295bb02e47e1c65346eb3
HUANG SIZHE <hsz1273327@gmail.com>
2024-04-23 09:59:35 +0800

    update

--- demo/app/beer-page.ts
+++
@@ -1,6 +0,0 @@
-import { EventData } from "@nativescript/core";
-
-// Event handler for Page "loaded" event attached in beer-page.xml
-export function pageLoaded(args: EventData) {
-  console.log("--- beers page loaded!");
-}
